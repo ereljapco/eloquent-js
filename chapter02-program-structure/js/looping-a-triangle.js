ab10e899c63ef2954fe55785f25addcbbaff8b72
Erel E. Ropeta-Japco <44773736+ereljapco@users.noreply.github.com>
2023-05-09 08:11:16 +0800

    rename: rename `NUM_OF_CALLS` for better readability

--- chapter02-program-structure/js/looping-a-triangle.js
+++ chapter02-program-structure/js/looping-a-triangle.js
@@ -1,5 +1,5 @@
-const CALLS = 7;
+const NUM_OF_CALLS = 7;
 
-for (let hash = '#'; hash.length <= CALLS; hash += '#') {
+for (let hash = '#'; hash.length <= NUM_OF_CALLS; hash += '#') {
   console.log(hash);
 }
