839a38dd0c6cab4117b54fe84d997cc86527a5a2
Erel E. Ropeta-Japco <44773736+ereljapco@users.noreply.github.com>
2023-05-11 07:44:34 +0800

    feat: add visualization for Chapter 02 - FizzBuzz exercise

--- chapter02-program-structure/js/fizzbuzz.js
+++ chapter02-program-structure/js/fizzbuzz.js
@@ -1,3 +1,5 @@
+// Original solution
+
 const MAX_NUMBER = 100;
 let numbers = '';
 
@@ -11,4 +13,29 @@ for (let i = 1; i <= MAX_NUMBER; i++) {
   } else {
     console.log(i);
   }
+}
+
+// Visualization
+
+const section = document.querySelector('.section');
+
+for (let i = 1; i <= MAX_NUMBER; i++) {
+  const num = document.createElement('span');
+
+  num.classList.add('fizzbuzz-num');
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    num.classList.add('fizzbuzz-num--fizzbuzz');
+    num.innerHTML = 'FizzBuzz';
+  } else if (i % 3 == 0) {
+    num.classList.add('fizzbuzz-num--fizz');
+    num.innerHTML = 'Fizz';
+  } else if (i % 5 == 0) {
+    num.classList.add('fizzbuzz-num--buzz');
+    num.innerHTML = 'Buzz';
+  } else {
+    num.innerHTML = i;
+  }
+
+  section.appendChild(num);
 }
