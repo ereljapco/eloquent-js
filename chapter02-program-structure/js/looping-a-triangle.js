c3f5e2d102ae59f588137360090a66e591cabbf6
Erel E. Ropeta-Japco <44773736+ereljapco@users.noreply.github.com>
2023-05-10 21:51:18 +0800

    feat: add a dynamic visualization for Chapter 02 - Looping a Triangle exercise

--- chapter02-program-structure/js/looping-a-triangle.js
+++ chapter02-program-structure/js/looping-a-triangle.js
@@ -1,3 +1,49 @@
+let sectionContainer = document.querySelector('#sectionContainer');
+let triangleForm = document.querySelector('#triangleForm');
+
+triangleForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  e.target.classList.remove('form--error');
+
+  let triangleSection = document.querySelector('.section--triangle');
+  let rowCount = document.querySelector('#rowCount').value;
+
+  if (triangleSection) {
+    sectionContainer.removeChild(triangleSection);
+  }
+
+  if (!rowCount) {
+    e.target.classList.add('form--error');
+
+    return;
+  }
+
+  triangleSection = document.createElement('section');
+
+  triangleSection.classList.add('section');
+  triangleSection.classList.add('section--triangle');
+
+  sectionContainer.appendChild(triangleSection);
+
+  for (let i = 0; i <= rowCount; i++) {
+    const row = document.createElement('div');
+    row.classList.add('triangle__row');
+
+    for (let j = 0; j < i; j++) {
+      const hashContainer = document.createElement('span');
+
+      hashContainer.classList.add('hash');
+      hashContainer.innerHTML = '#';
+      row.appendChild(hashContainer);
+    }
+
+    triangleSection.appendChild(row);
+  }
+});
+
+// Original solution
+
 const NUM_OF_CALLS = 7;
 
 for (let hash = '#'; hash.length <= NUM_OF_CALLS; hash += '#') {
